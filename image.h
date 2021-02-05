1fafa49b3385df18643536911ccd22a20748947a
burgers-ops <60606739+burgers-ops@users.noreply.github.com>
2021-02-05 09:11:18 +0700

    Add files via upload

---
+++ image.h
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#include <cstdio>
+
+enum ImageType {
+    PNG, JPG, BMP, TGA
+};
+
+struct Image {
+    uint8_t* data;
+    size_t size;
+    int w, h, channels;
+
+    Image(const char* filename);
+    Image(int w, int h, int channels);
+    Image(const Image& img);
+    ~Image();
+
+    bool read(const char* filename);
+    bool write(const char* filename);
+
+    ImageType getFileType(const char* filename);
+
+};
---
+++ itras.cpp
@@ -0,0 +1,14 @@
+#include "image.h"
+
+int main(int argc, char** argv) {
+    Image test("saudi.jpg");
+    test.write("new_saudi.png");
+    Image copy = test;
+    for (int i = 0; i < copy.w*copy.channels; ++i) {
+        copy.data[i] = 255;
+    }
+    copy.write("copy.png");
+    Image blank(100, 100, 3);
+    blank.write("blank.jpg");
+    return 0;
+}
